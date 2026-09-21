a0ebf02dea18a1570cdd09b346187d74213246b6
ZHIAN <chinarenzhian@gmail.com>
2026-09-21 22:32:42 +0200

    refactor: simplify Vercel entry point configuration
    Remove manual request URL normalization and wrapper functions in favor of direct export of the express application to improve routing performance and reliability on Vercel.

--- api/[...path].ts
+++
@@ -1,6 +0,0 @@
-import type { Request, Response } from 'express';
-import handler from './index';
-
-export default function catchAllHandler(req: Request, res: Response) {
-  return handler(req, res);
-}
